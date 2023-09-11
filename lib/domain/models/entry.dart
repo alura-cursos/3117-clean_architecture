67e90a2117f4c29b3344f300155e9c4f3a1b5f1e
ikyrie <mah.c5r@gmail.com>
2023-09-11 16:37:47 -0300

    video1.3

--- lib/domain/models/entry.dart
+++ lib/domain/models/entry.dart
@@ -35,9 +35,13 @@ class Entry {
       name: map['name'] as String,
       image: map['image'] as String,
       description: map['description'] as String,
-      commonLocations: map['commonLocations'] as String,
+      commonLocations: jsonEncode(map['common_locations'] ?? ['Sem localização']),
       category: map['category'] as String,
     );
+  }
+
+  List<String> commonLocationsConverter() {
+    return (jsonDecode(commonLocations) as List<dynamic>).map((e) => e as String).toList();
   }
 
   String toJson() => json.encode(toMap());
